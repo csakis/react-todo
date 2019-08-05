b894db5d6460c5a7d7c980121b3abc61bcb7c758
Csaba Kiss <csakis@lanl.gov>
2019-08-05 10:24:21 -0600

    added bulma components

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Button } from "react-bulma-components/full";
+
+class Todo extends Component {
+    state = {  }
+    render() { 
+        return (
+        <Button>Push me</Button> 
+          );
+    }
+}
+ 
+export default Todo;
